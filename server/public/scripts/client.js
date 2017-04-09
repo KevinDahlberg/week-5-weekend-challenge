20919f5e6dc0605958da4b8be4ca83a81f5b9c0b
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-04-09 01:01:58 -0500

    receiving movie from API and posting to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,15 +1,31 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('OneController', ['$scope', 'MovieService', function($scope, MovieService){
-
+  $scope.getOMDB = MovieService.getOMDB;
+  $scope.movieFavorites = MovieService.movieFavorites;
 }]);
 
 myApp.controller('TwoController', ['$scope', 'MovieService', function($scope, MovieService){
 
 }]);
 
-myApp.factory('MovieService', [function(){
+myApp.factory('MovieService', ['$http', function($http){
   var movieFavorites = [];
+
+function receivedMovie (movie) {
+   var newMovie =  {
+    Title : movie.Title,
+    Director : movie.Director,
+    Genre : movie.Genre,
+    Plot : movie.Plot,
+    Rated : movie.Rated,
+    Released : movie.Released,
+    Actors : movie.Actors,
+    Ratings : movie.Ratings,
+    Poster : movie.Poster
+  };
+  movieFavorites.push(newMovie);
+}
 
   function getNewMovie (object) {
     //get movie from DB here
@@ -23,7 +39,15 @@ myApp.factory('MovieService', [function(){
   }
 
   return {
-    movieFavorites : movieFavorites,
-    getNewMovie : getNewMovie
-  };
+  movieFavorites : movieFavorites,
+
+  getOMDB : function (movie) {
+    console.log(movie);
+    $http.get('http://www.omdbapi.com/?t=' + movie + '&y=&plot=full&r=json').then(function(response){
+      console.log(response);
+      receivedMovie(response.data);
+    });
+  }
+};
+
 }]);
